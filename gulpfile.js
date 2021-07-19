edd324881c0a28ec9f393f9351b2c87c54365ea1
Kainos-SU <kapinos4@gmail.com>
2021-07-20 01:46:08 +0300

    Gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const gulp = require("gulp");
 const rename = require("gulp-rename");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass")(require("sass"));
-const livereload = require("gulp-livereload");
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync");
 const fs = require("fs");
 const path = require("path");
 
@@ -18,17 +21,53 @@ function clearDir(dir){
     });
 }
 
-function build(cb) {
+function buildCSS(cb) {
+    console.log("CSS");
     const css = "css";
     clearDir(css);
     gulp.src('./src/scss/main.scss')
-        .pipe(sass({outputStyle: "compact"}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename("style.css"))
-        .pipe(gulp.dest(css));
+        .pipe(autoprefixer({
+            flexbox:true,
+            grid:true,
+        }))
+        .pipe(gulp.dest("." + path.sep + css));
 
     cb();
 }
 
+function buildHTML(cb) {
+    const indexPath = "." + path.sep + "index.html"
+    console.log(indexPath);
+    if (!fs.existsSync(indexPath)) {
+        console.log("There is no index.html");
+        cb();
+    }
+    gulp.src(indexPath)
+    .pipe(htmlmin({
+        collapseWhitespace:true,
+        collapseBooleanAttributes:true,
+        minifyCSS:true,
+        minifyJS:true,
+    }))
+    .pipe(gulp.dest("."+ path.sep + "dist"));
+    
+    cb();
+}
+
+function buildJS(cb) {
+    console.log("JS");
+    const jsPath = `.${path.sep}src${path.sep}js${path.sep}`;
+    if (!fs.existsSync(jsPath)) {
+        console.log("There is no JS sources");
+        cb();
+    }
+    gulp.src(`${jsPath}*.js`)
+        .pipe(minify())
+        .pipe(gulp.dest(`.${path.sep}dist${path.sep}js`))
+    cb();
+}
 
 function run (cb) {
     clearDir("css");
@@ -37,16 +76,26 @@ function run (cb) {
         .pipe(sourcemap.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemap.write("maps"))
-        .pipe(gulp.dest('css'))
-        .pipe(livereload.reload());
+        .pipe(gulp.dest('css'));
  
     cb();
 }
 
 
-exports.build = build;
+exports.build = gulp.series(buildHTML, buildCSS, buildJS);
+
+exports.css = buildCSS;
+    
 exports.run = ()=>{
-    livereload.listen({open: true, port:3000, reloadPage: "index.html", start: true});
+    run(console.log);
+
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        open: true,
+        notify: true
+    })
     gulp.watch('./src/**/*', run);
 
 };
